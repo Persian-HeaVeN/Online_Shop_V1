683776990bf9cbffc00249c63673c37c73dfd05b
HeaVeN <shayanlotfi1393@gmail.com>
2023-10-26 19:38:24 +0330

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -31,8 +31,8 @@ export function Nav() {
 
                 <a href='/' className="navbar-brand h1 mb-0 mx-auto">Shapaz Food</a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
                 </button>
 
             </div>
